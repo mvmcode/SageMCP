dcb2c0662f378211e4555cf89117e14a519da639
Manikandan (Mani) V M <manikandan.eshwar@gmail.com>
2025-10-12 07:46:55 -0700

    Add slack connector

--- src/sage_mcp/connectors/__init__.py
+++ src/sage_mcp/connectors/__init__.py
@@ -5,9 +5,9 @@ from .registry import ConnectorRegistry
 
 # Import all connector implementations to trigger registration
 from . import github  # noqa: F401
+from . import slack  # noqa: F401
 # TODO: Add other connectors as they are implemented
 # from . import gitlab
-# from . import slack
 # from . import discord
 # from . import custom
 
